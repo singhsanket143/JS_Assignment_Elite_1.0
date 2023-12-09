8443d6813fcbd1b828eaaf2c779ddc2166bf36de
Kritika Mishra <67709501+Kritikamishra-16@users.noreply.github.com>
2023-12-09 17:56:24 +0530

    Update question3.js

--- Problems/question3.js
+++ Problems/question3.js
@@ -44,5 +44,9 @@ sleep(2000).then(() => {
  * @return {Promise<void>}
  */
 export default async function sleep(duration) {
-    throw 'Not implemented';
+  return new Promise((resolve,reject)=>{
+    setTimeout(()=>{
+        resolve();
+    },duration);
+  })
   }
